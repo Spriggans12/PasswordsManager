040900b005a4d86b6fde207f44c8e19c43a827bf
Spriggans12 <vincent.justin12@gmail.com>
2018-09-17 11:14:31 +0200

    Renamed public to client

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const cn = {
 var db = pgp(cn);
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/client'));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
